1f13a9a10ca008ea7d52e5a10012484c49d9d19e
silvante <khamidov.ko@gmail.com>
2024-06-06 00:00:05 +0500

    fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [api, setapi] = useState([]);
+  async function getApi() {
+    const { data: response } = await axios.get("/api/places");
+    setapi(response);
+  }
+  useEffect(() => {
+    getApi();
+  }, []);
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-full flex justify-center px-16">
+      <div className="w-base">
+        <section>
+          {api.map((place) => {
+            return <div key={place._id}>{place.title}</div>;
+          })}
+        </section>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
