e1c3c372dfaea3b95e87088c2be419957b27bba3
silvante <khamidov.ko@gmail.com>
2024-06-25 17:55:06 +0500

    fix

--- client/src/components/SearchComponents.jsx
+++ client/src/components/SearchComponents.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchComponents = ({ model }) => {
   return (
-    <div className="bg-white fixed top-0 left-0 w-full h-screen z-50">
+    <div className="fixed top-0 left-0 w-full h-screen bg-white z-50 overflow-y-scroll scroll-none">
       <header className="w-full bg-white py-5 px-5 flex justify-between items-center fixed top-0">
         <button
           className="flex items-center text-xl"
@@ -11,6 +11,11 @@ const SearchComponents = ({ model }) => {
           <i className="bx bx-chevron-left text-3xl"></i>exit
         </button>
       </header>
+      <div>
+        <form>
+          <input type="text" className="" placeholder="place" />
+        </form>
+      </div>
     </div>
   );
 };
