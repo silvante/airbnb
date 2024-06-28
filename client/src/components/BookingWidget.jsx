87b1961ae38b61ad95169ce4c9e4735181a3bb8e
silvante <khamidov.ko@gmail.com>
2024-06-28 11:57:47 +0500

    fix

--- client/src/components/BookingWidget.jsx
+++ client/src/components/BookingWidget.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { IMaskInput } from 'react-imask';
 
-const BookingWidget = ({place}) => {
+const BookingWidget = ({ place }) => {
   return (
     <div className="bg-white p-5 rounded-xl text-center space-y-3">
       <div>
@@ -11,7 +12,31 @@ const BookingWidget = ({place}) => {
           maximum number of guests {place.maxGuests}
         </p>
       </div>
-      <button className="w-full bg-base text-white rounded-lg py-3">
+      <form className="flex flex-col text-start space-y-2">
+        <div className="space-y-1">
+          <label>number of guests</label>
+          <input
+            type="number"
+            max={place.maxGuests}
+            placeholder="number here"
+            className="bg-fun w-full outline-none py-2 px-4 border-b-2 border-slate-300 rounded-lg"
+          />
+        </div>
+        <div className="space-y-1">
+          <label>your phone number</label>
+          <IMaskInput
+            mask="(#00) 00-000-0000"
+            definitions={{
+              "#": /[1-9]/,
+            }}
+            onAccept={(value) =>
+              onChange({ target: { name: props.name, value } })
+            }
+            overwrite
+          />
+        </div>
+      </form>
+      <button className="w-full bg-base text-white rounded-lg py-2">
         Request
       </button>
     </div>
