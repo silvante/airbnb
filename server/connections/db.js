12fdd523a5ea0fe2b5a854e5c12132c9329ba1de
silvante <khamidov.ko@gmail.com>
2024-07-09 23:11:11 +0500

    fix

--- server/connections/db.js
+++ server/connections/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDataBase = async () => {
   try {
-    const connect = await mongoose.connect(process.env.DATABASE_URL);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`mongodb connected to ${connect.connection.host}`);
   } catch (err) {
     console.log(err);
