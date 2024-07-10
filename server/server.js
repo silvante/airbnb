15484913b3eceb8045afcdfe8b60eca0f61d1d46
silvante <khamidov.ko@gmail.com>
2024-07-11 01:02:58 +0500

    fix

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airwayuz.netlify.app/",
   })
 );
 app.use(express.json());
