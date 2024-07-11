21d416f2a3b60a4e7a4fc4ce644311703345bf4f
silvante <khamidov.ko@gmail.com>
2024-07-11 16:03:26 +0500

    fix

--- client/src/components/HostBooking.jsx
+++ client/src/components/HostBooking.jsx
@@ -10,7 +10,10 @@ const HostBooking = ({ owner }) => {
   return (
     <div className="space-y-5">
       <h3 className="text-xl font-semibold">Meet your Host</h3>
-      <Link to={`/user/${owner.username}`} className="flex flex-col bg-white rounded-2xl w-[400px] shadow-xl px-5 py-10 justify-center items-center space-y-3">
+      <Link
+        to={`/user/${owner.username}`}
+        className="flex flex-col bg-white rounded-2xl w-[400px] shadow-xl px-5 py-10 justify-center items-center space-y-3"
+      >
         <div className=" relative">
           <img
             src={imageTotalLink + owner.avatar}
@@ -19,12 +22,14 @@ const HostBooking = ({ owner }) => {
             width={"110px"}
             height={"110px"}
           />
-          <img
-            src={check}
-            alt="verify icon"
-            width={"30px"}
-            className="absolute bottom-0 right-0"
-          />
+          {owner.verificated && (
+            <img
+              src={check}
+              alt="verify icon"
+              width={"30px"}
+              className="absolute bottom-0 right-0"
+            />
+          )}
         </div>
         <div className="text-center">
           <h3 className="text-2xl font-semibold truncate">{owner.name}</h3>
